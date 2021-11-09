b8b5b99b1209f6610067aef825d89eb0167eac93
yektas <ahmetsercanyektas@gmail.com>
2021-11-09 12:08:59 +0300

    connect with wallet

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Web3ReactProvider } from "@web3-react/core";
+import Web3 from "web3";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function getLibrary(provider) {
+  return new Web3(provider);
 }
 
-export default MyApp
+function MyApp({ Component, pageProps }) {
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
+  );
+}
+
+export default MyApp;
